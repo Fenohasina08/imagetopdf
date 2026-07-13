4653e376e6cecbf0867c9bf268a1fd1db11b1ccc
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-13 09:43:59 +0300

    feat: content of the entity

---
+++ src/main/java/com/example/demo/entity/ImageMetadata.java
@@ -0,0 +1,43 @@
+package com.example.demo.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "image_metadata")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ImageMetadata {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "file_name", nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+     @Column(name = "original_bucket_key", nullable = false)
+    private String originalBucketKey;
+
+     @Column(name = "transformed_bucket_key", nullable = false)
+    private String transformedBucketKey;
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,30 @@
+package com.example.demo.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
