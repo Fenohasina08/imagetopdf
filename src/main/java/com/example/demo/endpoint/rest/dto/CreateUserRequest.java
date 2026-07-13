7ad539a5f1c3dce9ef3fc7e4c0b60ea3679836fd
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-13 09:45:45 +0300

    feat: content of the createUserRequest

---
+++ src/main/java/com/example/demo/endpoint/rest/dto/CreateUserRequest.java
@@ -0,0 +1,19 @@
+package com.example.demo.endpoint.rest.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserRequest {
+
+    @NotBlank
+    @Email
+    private String email;
+}
