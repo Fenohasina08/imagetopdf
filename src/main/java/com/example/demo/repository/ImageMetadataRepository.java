4751d3b3808317055eef5ab4ea01edc7ab6e5aae
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-07-13 09:45:08 +0300

    feat: content of the repository of Imagemetadata

---
+++ src/main/java/com/example/demo/repository/ImageMetadataRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import com.example.demo.domain.entity.ImageMetadata;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageMetadataRepository extends JpaRepository<ImageMetadata, UUID> {
+
+}
